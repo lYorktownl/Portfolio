0cdd5b290455a73b8291b8c3cb377b5a3cc69c38
Yorktown <example@mail.com>
2024-01-17 09:37:43 +0700

    fixed icons paths

--- src/components/SideCard.vue
+++ src/components/SideCard.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="sidebar border-2 mb-4 xl:w-64 w-auto">
     <div class="flex justify-center m-7">
-      <img class="h-60 m-5" src="../../public/assets/avatar.jpg" alt="my photo" />
+      <img class="h-60 m-5" src="/assets/avatar.jpg" alt="my photo" />
     </div>
     <hr class="w-3/4 m-auto" />
     <div class="m-3 flex justify-center">
       <ul class="font-bold">
         <li class="flex text-lg">
-          <img src="../../public/assets/icons/location.svg" class="w-10" />Novosibirsk
+          <img src="/assets/icons/location.svg" class="w-10" />Novosibirsk
         </li>
-        <li class="flex text-lg"><img src="../../public/assets/icons/badge.svg" class="w-10" />23 y.o</li>
+        <li class="flex text-lg"><img src="/assets/icons/badge.svg" class="w-10" />23 y.o</li>
         <li class="flex text-lg">
-          <img src="../../public/assets/icons/telegram.svg" class="w-7 ml-[5px]" /><a
+          <img src="/assets/icons/telegram.svg" class="w-7 ml-[5px]" /><a
             :href="'https://t.me/N0tAlone'"
             target="_blank"
             class="pl-2 no-underline hover:underline"
